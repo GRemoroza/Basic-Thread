cb9da4674c4dd05b696c9fc31499cbb47d4d0752
remoroza.254068 <remoroza.254068@LAB-A-PC27>
2023-10-20 18:53:16 +0800

    Add project files.

---
+++ Basic Thread/MyThreadClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basic_Thread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for(int t1 = 0; t1 <= 5; t1++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + t1);
+                Thread.Sleep(1500);
+            }
+         }
+      
+    }
+    
+}
